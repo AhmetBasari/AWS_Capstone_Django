3e06edfdacf5378883f317c96e86575b3d6f9883
Ahmet Basari <ahmetsuccess@gmail.com>
2024-06-21 09:15:26 +0200

    secondfile

--- Solution_Instructor/userdata.sh
+++ Solution_Instructor/userdata.sh
@@ -7,12 +7,12 @@ apt install python3-pip -y
 pip3 install boto3
 apt  install awscli -y
 cd /home/ubuntu/
-TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /<yourname>/capstone/token --with-decryption --query 'Parameter.Value' --output text)
-git clone https://$TOKEN@github.com/<yourreponame>/aws-capstone-project.git
-cd /home/ubuntu/aws-capstone-project
+TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /Ahmet/Capstone/token --with-decryption --query 'Parameter.Value' --output text)
+git clone https://$TOKEN@github.com/AhmetBasari/Capstone-Django.git
+cd /home/ubuntu/Capstone-Django
 apt-get install python3.10-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/aws-capstone-project/src
+cd /home/ubuntu/Capstone-Django/src
 python3 manage.py collectstatic --noinput
 #python3 manage.py makemigrations
 #python3 manage.py migrate
